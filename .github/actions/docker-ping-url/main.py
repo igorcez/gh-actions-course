126525f9f6a4890e8608a2b8c4d1d6f71643ed9b
Igor Ceriotti Zilio <igorceriottizilio@Igors-MacBook-Air.local>
2026-07-28 20:33:41 -0300

    feat: actions docker

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -1,2 +1,33 @@
+import requests
+import time
+import os
+
 if __name__ == "__main__":
-    print("Hello world")
+    run()
+
+def ping_url (url, delay, max_trials):
+    number_of_trials = 0
+    while number_of_trials < max_trials:
+        resp = requests.get(f"{url}", timeout=5)
+        print(resp.status_code, resp.text)
+        if resp.status_code == 200:
+            return True
+        else:
+            time.sleep(delay)
+            number_of_trials += 1
+    return False
+
+def run():
+    url = os.getenv("INPUT_URL")
+    delay = os.getenv("INPUT_DELAY")
+    max_trials = os.getenv("INPUT_MAX_TRIALS")
+
+    result = ping_url(url, delay, max_trials)
+    if result == False:
+        raise RuntimeError("something went wrong") 
+
+
+
+        
+
+
